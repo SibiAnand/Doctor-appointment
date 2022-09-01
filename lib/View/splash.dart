bdd9d6f900dd25c271ff054df14d14634d316ade
Sibi Anand <sibianand5662@gmail.com>
2022-09-01 16:47:40 +0530

    splash

---
+++ lib/View/splash.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class splash extends StatefulWidget {
+  const splash({Key? key}) : super(key: key);
+
+  @override
+  State<splash> createState() => _splashState();
+}
+
+class _splashState extends State<splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+    
+  }
+}
